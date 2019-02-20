ca6f9ff482924bef68d5ff1d2dbeff46a6d60d99
r.voronin <genom90@gmail.com>
2019-02-20 23:49:08 +0300

    Рефакторинг

--- VkTools/Serializers/NewsFeedDeserializer.cs
+++ VkTools/Serializers/NewsFeedDeserializer.cs
@@ -12,8 +12,66 @@ using VkTools.ObjectModel.Wall;
 
 namespace VkTools.Serializers
 {
-    public partial class NewsFeedSerializer
+    public class NewsFeedDeserializer
     {
+        #region consts
+        public const string PResponse = "response";
+        public const string PItems = "items";
+        public const string PItemType = "type";
+        public const string PItemDate = "date";
+        public const string PItemId = "post_id";
+        public const string PItemText = "text";
+        public const string PItemSignerId = "signer_id";
+        public const string PItemMarkedAsAds = "marked_as_ads";
+        public const string PAttachments = "attachments";
+        public const string PAttachmentsType = "type";
+        public const string PAttachmentsPhoto = "photo";
+        public const string PAttachmentsDocument = "doc";
+        public const string PAttachmentsAudio = "audio";
+        public const string PAttachmentsVideo = "video";
+        public const string PAttachmentsLink = "link";
+        public const string PPhotoAlbumId = "album_id";
+        public const string PAttachmentOwnerId = "owner_id";
+        public const string PId = "id";
+        public const string PUrl = "url";
+        public const string PDate = "date";
+        public const string PTitle = "title";
+        public const string PPhotoUserId = "user_id";
+        public const string PPhotoText = "text";
+        public const string PPhotoSizes = "sizes";
+        public const string PSizesType = "type";
+        public const string PSizesWidth = "width";
+        public const string PSizesHeight = "height";
+        public const string PComments = "comments";
+        public const string PCommentsCount = "count";
+        public const string PCommentsCanPost = "can_post";
+        public const string PCommentsGroupsCanPost = "groups_can_post";
+        public const string PLikes = "likes";
+        public const string PLikesCount = "count";
+        public const string PLikesUserLikes = "user_likes";
+        public const string PLikesCanLike = "can_like";
+        public const string PLikesCanPublish = "can_publish";
+        public const string PReposts = "reposts";
+        public const string PRepostsCount = "count";
+        public const string PRepostsUserReposted = "user_reposted";
+        public const string PViews = "views";
+        public const string PViewsCount = "count";
+        public const string PIsFavorite = "is_favorite";
+        public const string PPostSource = "post_source";
+        public const string PPostSourceType = "type";
+        public const string PPostSourcePlatform = "platform";
+        public const string PPostSourceData = "data";
+        public const string PVideoDescription = "description";
+        public const string PVideoDuration = "duration";
+        public const string PVideoViews = "views";
+        public const string PVideoComments = "comments";
+        public const string PVideoPlayer = "player";
+        public const string PVideoIsFavorite = "is_favorite";
+        public const string PAudioArtist = "artist";
+        public const string PLinkDescription = "description";
+        public const string PAttachmentAccessKey = "access_key";
+        #endregion
+
         public NewsFeed Deserialize(string _data)
         {
             var jObject = JObject.Parse(_data);
