08d0b7d9a0a8f6817c7b9db4f8e377917d2e1531
mrgoner <genom90@gmail.com>
2022-10-29 15:47:04 +0300

    added docker support

---
+++ ConsoleBot/Program.cs
@@ -0,0 +1,24 @@
+﻿using TelegramBot;
+
+var token = Environment.GetEnvironmentVariable("grabber_token");
+var dbFolder = Environment.GetEnvironmentVariable("grabber_db_path");
+
+if(string.IsNullOrWhiteSpace(token))
+    throw new ArgumentException(nameof(token));
+
+if(string.IsNullOrWhiteSpace(dbFolder))
+    throw new ArgumentException(nameof(dbFolder));
+
+var bot = new Bot(token, dbFolder);
+
+var cancellationTokenSource = new CancellationTokenSource();
+
+await bot.Start(cancellationTokenSource.Token);
+
+Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) => 
+{
+    cancellationTokenSource.Cancel();
+    Environment.Exit(0);
+};
+
+Thread.Sleep(-1);
